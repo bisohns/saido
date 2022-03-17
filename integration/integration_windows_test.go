654167a32db403a09a1379158a0e2bea91e8f400
Diretnan Domnan <diretnandomnan@gmail.com>
2022-03-17 19:28:03 +0100

    Adding customlocaltest on windows

--- integration/integration_windows_test.go
+++ integration/integration_windows_test.go
@@ -1,6 +1,7 @@
 package integration
 
 import (
+	"strings"
 	"testing"
 
 	"github.com/bisohns/saido/driver"
@@ -22,6 +23,21 @@ func TestProcessonLocal(t *testing.T) {
 		}
 		if process := iConcreteWin.Values[0].Command; process != "System Idle Process" {
 			t.Errorf("Expected System Idle Process as first process, found %s", iConcreteWin.Values[0].Command)
+		}
+	}
+}
+
+func TestCustomonLocal(t *testing.T) {
+	d := NewLocalForTest()
+	dfConcrete, _ := d.(*driver.Local)
+	dfConcrete.Vars = []string{"EXAMPLES=true"}
+	d = dfConcrete
+	i, _ := inspector.Init(`custom`, &d, `echo %EXAMPLES%`)
+	i.Execute()
+	iConcrete, ok := i.(*inspector.Custom)
+	if ok {
+		if strings.TrimSpace(iConcrete.Values.Output) != "true" {
+			t.Errorf("Expected 'true', found %s", iConcrete.Values.Output)
 		}
 	}
 }
