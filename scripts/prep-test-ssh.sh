1213d36cbc2766c099a19f18ea0867aeb05b6acc
Mmadu Manasseh <mmadumanasseh@gmail.com>
2022-03-20 21:17:39 +0100

    docker host

--- scripts/prep-test-ssh.sh
+++ scripts/prep-test-ssh.sh
@@ -9,13 +9,13 @@ docker kill saido-linux-sshserver | true
 docker run -d -p 2222:2222 -e USER_NAME=$SSH_USER --name saido-linux-sshserver -v ${SSH_KEY_PATH}/${SSH_KEY_NAME}.pub:/config/.ssh/authorized_keys linuxserver/openssh-server
 cat <<EOF > config-ci.yaml
 hosts:
-  connection:
-    type: ssh
-    port: 2222
-    username: ${SSH_USER}
-    private_key_path: $(pwd)/${SSH_KEY_NAME}
   children:
     "$(docker inspect -f "{{ .NetworkSettings.IPAddress }}" saido-linux-sshserver)":
+      connection:
+        type: ssh
+        port: 2222
+        username: ${SSH_USER}
+        private_key_path: $(pwd)/${SSH_KEY_NAME}
     "127.0.0.1":
       connection:
         type: local
