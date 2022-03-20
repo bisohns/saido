acbb6e116a8c859a5c94cacb99746c809d84b9c6
Mmadu Manasseh <mmadumanasseh@gmail.com>
2022-03-20 21:05:01 +0100

    Fix docker mounts

--- scripts/prep-test-ssh.sh
+++ scripts/prep-test-ssh.sh
@@ -1,19 +1,19 @@
 # Creates a docker container with ssh and a config file for connecting to it
 SSH_KEY_NAME=${SSH_KEY_NAME:-ci-test-key}
-SSH_KEY_PATH=ssh-key/ci
+SSH_KEY_PATH=$(pwd)/ssh-key/ci
 SSH_USER=ci-dev
 rm -rf $SSH_KEY_PATH
 mkdir -p $SSH_KEY_PATH
 ssh-keygen -f "$SSH_KEY_PATH/${SSH_KEY_NAME}" -N ""
 docker kill saido-linux-sshserver | true
-docker run -d -p 2222:2222 -e USER_NAME=$SSH_USER --name saido-linux-sshserver -v $(pwd)/ci.pub:/config/.ssh/authorized_keys linuxserver/openssh-server
+docker run -d -p 2222:2222 -e USER_NAME=$SSH_USER --name saido-linux-sshserver -v ${SSH_KEY_PATH}/${SSH_KEY_NAME}.pub:/config/.ssh/authorized_keys linuxserver/openssh-server
 cat <<EOF > config-ci.yaml
 hosts:
 	connection: 
 		type: ssh
 		username: ${SSH_USER}
     port: 2222
-		private_key_path: $(pwd)/${SSH_KEY_NAME}.pub
+		private_key_path: $(pwd)/${SSH_KEY_NAME}
 	children:
 		"$(docker inspect -f "{{ .NetworkSettings.IPAddress }}" saido-linux-sshserver)":
 		"127.0.0.1":
