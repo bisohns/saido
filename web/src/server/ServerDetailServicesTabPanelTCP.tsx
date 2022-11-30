512c15e073d732a66d641cf528389de031dbf305
Nurudeen Yekeen <nyekeen@lunchbox.io>
2022-11-30 02:36:08 +0100

    impl TCP table integration

--- web/src/server/ServerDetailServicesTabPanelTCP.tsx
+++ web/src/server/ServerDetailServicesTabPanelTCP.tsx
@@ -1,9 +1,13 @@
-import Table from "common/Table";
-import useTable from "common/useTable";
-import React from "react";
+import Table from 'common/Table';
+import useTable from 'common/useTable';
+import React, { useMemo } from 'react';
 import { getCoreRowModel } from '@tanstack/react-table';
-import { ServerResponseType, ServerServiceNameType, TCPData } from "./ServerType";
-import { useVirtual } from "react-virtual";
+import {
+  ServerResponseType,
+  ServerServiceNameType,
+  TCPData,
+} from './ServerType';
+import { useVirtual } from 'react-virtual';
 
 interface ServerDetailServicesTabPanelTCPType {
   serverName: ServerServiceNameType;
@@ -13,44 +17,50 @@ interface ServerDetailServicesTabPanelTCPType {
 export default function ServerDetailServicesTabPanelTCP(
   props: ServerDetailServicesTabPanelTCPType
 ) {
-   const {
-     serverData: {
-       Message: { Data: {Ports } },
-     },
-   } = props;
+  const {
+    serverData: {
+      Message: {
+        Data: { Ports },
+      },
+    },
+  } = props;
 
-   const data = Object.entries(Ports).map(([key, value]) => {
-      return {
-        Port: key,
-        State: value,
-      };
-   })
+  const data = useMemo(
+    () =>
+      Object.entries(Ports).map(([key, value]) => {
+        return {
+          Port: key,
+          State: value,
+        };
+      }),
+    [Ports]
+  );
 
-   const tableInstance = useTable({
-     data,
-     columns,
-     getCoreRowModel: getCoreRowModel(),
-   });
+  const tableInstance = useTable({
+    data,
+    columns,
+    getCoreRowModel: getCoreRowModel(),
+  });
 
-   const tableContainerRef = React.useRef<HTMLDivElement>(null);
+  const tableContainerRef = React.useRef<HTMLDivElement>(null);
 
-   const { rows } = tableInstance.getRowModel();
+  const { rows } = tableInstance.getRowModel();
 
-   const rowVirtualizer = useVirtual({
-     parentRef: tableContainerRef,
-     size: rows.length,
-     overscan: 10,
-   });
+  const rowVirtualizer = useVirtual({
+    parentRef: tableContainerRef,
+    size: rows.length,
+    overscan: 10,
+  });
 
-    return (
-      <Table
-        ref={tableContainerRef}
-        variant='default'
-        virtualization
-        instance={tableInstance}
-        virtualizationInstance={rowVirtualizer}
-      />
-    );
+  return (
+    <Table
+      ref={tableContainerRef}
+      variant='default'
+      virtualization
+      instance={tableInstance}
+      virtualizationInstance={rowVirtualizer}
+    />
+  );
 }
 
 const columns = [
